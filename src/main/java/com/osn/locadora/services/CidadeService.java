09dde31c3e1ba0ed9b7fac00f5d4a4441556ea65
Osvando Neto <osvaldosneto@hotmail.com>
2020-07-13 21:28:38 -0300

    implementando filter show location

--- src/main/java/com/osn/locadora/services/CidadeService.java
+++ src/main/java/com/osn/locadora/services/CidadeService.java
@@ -1,6 +1,7 @@
 package com.osn.locadora.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,12 @@ public class CidadeService {
 	
 	@Autowired
 	private CidadeRepository repo;
+	
+	public Cidade find(Long id) {
+		Optional<Cidade> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new com.osn.locadora.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cidade.class.getName()));
+	}
 	
 	public List<Cidade> findByEstado(Long estadoId) {
 		return repo.findCidades(estadoId);
