5da7ecbe608e96410ddaa4a099d94f85ef3cf238
Osvando Neto <osvaldosneto@hotmail.com>
2020-08-05 08:55:27 -0300

    upload heroku

--- src/main/java/com/osn/locadora/config/SecutiryConfig.java
+++ src/main/java/com/osn/locadora/config/SecutiryConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,11 +30,19 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private JWTUtil jwtUtil;
-
+	
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/clientes/**",
+			"/hospedagens/**",
+			"/reservas/**"
+	};
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
