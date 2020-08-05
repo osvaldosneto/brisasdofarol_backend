1befd5e56179ce014e8472a66a2c792e7fa416b7
Osvando Neto <osvaldosneto@hotmail.com>
2020-08-05 08:25:50 -0300

    upload heroku

--- src/main/java/com/osn/locadora/config/DevConfig.java
+++ src/main/java/com/osn/locadora/config/DevConfig.java
@@ -1,26 +1,15 @@
 package com.osn.locadora.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.osn.locadora.services.DBService;
 import com.osn.locadora.services.EmailService;
 import com.osn.locadora.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
-
-	@Autowired
-	private DBService dbService;
-
-	@Bean
-	public boolean instantiatedDatabase(){
-		dbService.instanciatedDevDatabase();
-		return true;
-	}
 	
 	@Bean
 	public EmailService emailService() {
