fe4dc1e737b15ba10e38d591c0bc2efe73311c25
Osvando Neto <osvaldosneto@hotmail.com>
2020-08-08 15:32:22 -0300

    documentação swagger

--- src/main/java/com/osn/locadora/config/SecutiryConfig.java
+++ src/main/java/com/osn/locadora/config/SecutiryConfig.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,20 +30,23 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private JWTUtil jwtUtil;
-	
-	
-	private static final String[] PUBLIC_MATCHERS = {
-			"/clientes/**",
-			"/hospedagens/**",
-			"/reservas/**"
-	};
-	
+
+	private static final String[] PUBLIC_MATCHERS = { "/clientes/**", "/hospedagens/**", "/reservas/**" };
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", 
+				"/configuration/ui", 
+				"/swagger-resources/**", 
+				"/configuration/**",
+				"/swagger-ui.html", 
+				"/webjars/**");
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
